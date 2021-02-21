af6b8a28294ba93b2f96c409c7b0eea6d6b876d8
jestin-g <jestingabriel@gmail.com>
2021-02-21 20:42:01 +0100

    Add Services OK

--- src/app/message.service.ts
+++ src/app/message.service.ts
@@ -8,4 +8,12 @@ export class MessageService {
   messages: string[] = [];
 
   constructor() { }
+
+  add(message: string): void {
+    this.messages.push(message);
+  }
+
+  clear(): void {
+    this.messages = []
+  }
 }
